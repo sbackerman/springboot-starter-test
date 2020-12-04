d241b6ff1bf7d5448d59a77578dcca8cd32ec576
zhengzhaoxiang <1059311525@qq.com>
2020-12-04 10:06:41 +0800

    初始化 springboot starter 测试

---
+++ demo3_sql_jdbc/src/main/java/com/example/demo/Demo3SqlJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo3SqlJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo3SqlJdbcApplication.class, args);
+    }
+
+}
---
+++ demo6_sql_mybatis/src/main/java/com/example/demo6_sql_mybatis/Demo6SqlMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo6_sql_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo6SqlMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo6SqlMybatisApplication.class, args);
+    }
+
+}
